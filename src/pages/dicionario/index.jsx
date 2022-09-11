6c4e0e2c9ada0eec737bf7f206491bcb4254d5c3
Adriano <adriano_alves_2008@hotmail.com>
2022-09-11 09:55:15 -0300

    inicio do submit

--- src/pages/dicionario/index.jsx
+++ src/pages/dicionario/index.jsx
@@ -8,29 +8,44 @@ function DicionarioPage() {
 
   const [showPostModal, setShowPostModal] = useState(false)
   const [detail, setDetail] = useState()
+
+  const [palavra, setPalavra] =useState('')
+  const [traducao, setTraducao] =useState('')
+  const [uso, setUso] =useState('')
+
   
-  function togglePostModal (item){
+  function togglePostModal (){
     setShowPostModal(!showPostModal)
-    setDetail(item)
+    setDetail()
   }
+
+  function enviarPalavra (e){
+    e.preventDefault()
+    console.log(traducao)
+
+    setPalavra('')
+    setTraducao('')
+    setUso('')
+  }
+
   return (
     <div className="tabela-content">
-      <form className='formulario'>
+      <form className='formulario' onSubmit={enviarPalavra}>
         <h2>Registre uma palavra</h2>
         <div className='campo-input'>
           <label htmlFor="palavra">Palavra</label>
-          <input id='palavra' type="text" />
+          <input id='palavra' type="text" value={palavra} onChange={e => setPalavra(e.target.value)} />
         </div>
         <div className='campo-input'>
           <label htmlFor="traducao">Tradução</label>
-          <input id='traducao' type="text" />
+          <input id='traducao' type="text" value={traducao} onChange={e => setTraducao(e.target.value)} />
         </div>
         <div className='campo-input'>
           <label htmlFor="aplicacao">Aplicação em frase</label>
-          <input id='aplicacao' type="text" />
+          <input id='aplicacao' type="text" value={uso} onChange={e => setUso(e.target.value)} />
         </div>
 
-        <button>Cadastrar palavra</button>
+        <button type='submit'>Cadastrar palavra</button>
       </form> 
         <table className="tabela">
           <thead>
@@ -41,15 +56,12 @@ function DicionarioPage() {
             </tr>
           </thead>
           <tbody className="tabela-dicionario">
-            {dicionario.map(item=>{
-              return(
                 <tr>
-                  <td>{item.Palavra}</td>
-                  <td>{item.Traducao}</td>
-                  <button onClick={(()=> togglePostModal(item))}>Aplicação em frase</button>
+                  <td>{palavra}</td>
+                  <td>{traducao}</td>
+                  <button onClick={(()=> togglePostModal())}>Aplicação em frase</button>
                 </tr>
-              )
-            })}
+           
           </tbody>
         </table>
         {showPostModal && (
