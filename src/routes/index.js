821efaefcc0dab3058d27cd974e5c4a2afd840b5
Adriano <adriano_alves_2008@hotmail.com>
2022-12-16 21:34:04 -0300

    criando rota privada

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,21 @@ import DicionarioPage from '../pages/dicionario'
 import Register from '../pages/Register'
 import Home from '../pages/Home'
 
+import Private from './Private'
+
 function RoutesApp() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dicionario" element={<DicionarioPage />} />
+      <Route
+        path="/dicionario"
+        element={
+          <Private>
+            <DicionarioPage />
+          </Private>
+        }
+      />
     </Routes>
   )
 }
